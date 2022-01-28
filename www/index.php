0d3e3366578f7bdbb2b0e5d1e484faef49d23503
Antonio Moreno <jersonmartinezsm@gmail.com>
2022-01-28 14:30:50 -0600

    Changing order and adding new configation about connection to the DB

---
+++ www/index.php
@@ -0,0 +1,36 @@
+<html>
+    <head>
+        <title>Welcome to LAMP Infrastructure</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <?php
+                echo "<h1>¡Hola, OpenWebinars te da la bienvenida!</h1>";
+
+                $conn = mysqli_connect('db', 'root', 'test', "dbname");
+
+                $query = 'SELECT * From Person';
+                $result = mysqli_query($conn, $query);
+
+                echo '<table class="table table-striped">';
+                echo '<thead><tr><th></th><th>id</th><th>name</th></tr></thead>';
+                while($value = $result->fetch_array(MYSQLI_ASSOC)){
+                    echo '<tr>';
+                    echo '<td><a href="#"><span class="glyphicon glyphicon-search"></span></a></td>';
+                    foreach($value as $element){
+                        echo '<td>' . $element . '</td>';
+                    }
+
+                    echo '</tr>';
+                }
+                echo '</table>';
+
+                $result->close();
+                mysqli_close($conn);
+            ?>
+        </div>
+    </body>
+</html>
